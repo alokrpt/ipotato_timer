10d83853c9b65783c789281b8c0323128e6ec9a6
Alok Singh <alokrpt@gmail.com>
2024-03-02 16:37:30 +0530

    added dependency injection and text styles

---
+++ lib/src/core/dependency_injection/injection_container.dart
@@ -0,0 +1,52 @@
+import 'package:get_it/get_it.dart';
+import 'package:ipotato_timer/src/features/add_task/presentation/pages/widgets/duration_input/duration_input_store.dart';
+
+import '../../features/add_task/data/datasources/add_task_data_source.dart';
+import '../../features/add_task/data/repositories/add_task_repository_impl.dart';
+import '../../features/add_task/domain/repositories/add_task_repository.dart';
+import '../../features/add_task/domain/usecases/add_task_use_case.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initDependencyInjection() async {
+  // Store
+  _storeSl();
+
+  // Data sources
+  _dataSourcesSl();
+
+  // Repository
+  _repositorySl();
+
+  // Use cases
+  _useCasesSl();
+
+  // Core
+  _coreSl();
+}
+
+void _storeSl() {
+  sl.registerLazySingleton(
+    () => DurationInputStore(),
+  );
+}
+
+void _dataSourcesSl() {
+  sl.registerLazySingleton<AddTaskDataSource>(
+    () => AddTaskDataSourceImpl(sl()),
+  );
+}
+
+void _repositorySl() {
+  sl.registerLazySingleton<AddTaskRepository>(
+    () => AddTaskRepositoryImpl(dataSource: sl()),
+  );
+}
+
+void _useCasesSl() {
+  sl.registerLazySingleton<AddTaskUseCase>(
+    () => AddTaskUseCase(sl()),
+  );
+}
+
+void _coreSl() {}
---
+++ lib/src/core/presentation/text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract final class TextStyles {
+  static const pageTitle = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+  );
+  static const labelText = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+  static const actionText = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+}
